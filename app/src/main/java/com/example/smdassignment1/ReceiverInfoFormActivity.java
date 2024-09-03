b82b0683118186eb1c6fbd910d799e495c488b3c
jawadshahid07 <jawad.shahid70@outlook.com>
2024-09-03 20:04:45 +0500

    created sender and receiver info forms

--- app/src/main/java/com/example/smdassignment1/ReceiverInfoFormActivity.java
+++ app/src/main/java/com/example/smdassignment1/ReceiverInfoFormActivity.java
@@ -6,19 +6,19 @@ import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SenderInfoFormActivity extends AppCompatActivity {
+public class ReceiverInfoFormActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) { // on the event that this activity is created
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sender_info_form);
+        setContentView(R.layout.activity_receiver_info_form);
 
         Button nextButton = findViewById(R.id.btnNext); // use hooks to find the button
         nextButton.setOnClickListener(new View.OnClickListener() { // when button is clicked, next activity should be displayed
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SenderInfoFormActivity.this, ReceiverInfoFormActivity.class); // use intent to display next activity
-                startActivity(intent);
+                //Intent intent = new Intent(ReceiverInfoFormActivity.this, ReviewInfoActivity.class); // use intent to display next activity
+                //startActivity(intent);
                 finish();
             }
         });
